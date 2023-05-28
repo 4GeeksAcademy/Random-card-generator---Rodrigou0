1584c3411a19493f28287720c45b86695d304b4d
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-05-28 12:48:46 +0000

    card deck

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuits());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuits = () => {
+  let suits = ["diamond", "spade", "club", "heart"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
